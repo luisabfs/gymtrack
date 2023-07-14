435f839211e519749d6638fea60f5b9baf4ac716
Luisa Barros <luisa.barros.dev@gmail.com>
2023-07-14 14:31:16 -0300

    feat[WIP]: add first theming implementation

---
+++ styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import Theme from './src/themes';
+
+export type ThemeType = typeof Theme;
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
