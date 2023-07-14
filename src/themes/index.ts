8d67863282f49b520ae9c2fd357b1462335f2185
Luisa Barros <luisa.barros.dev@gmail.com>
2023-07-14 14:31:16 -0300

    feat[WIP]: add first theming implementation

---
+++ src/themes/index.ts
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        background: '#333333',
+        accent: '#DF1265',
+        fonts: {
+            primary: '#F2F2F2',
+            secondary: '#828282',
+        },
+    }
+};
+
+export default theme;
