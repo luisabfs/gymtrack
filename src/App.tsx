158004a9a9d8051f01036ca89ee2aef134a84d28
Luisa Barros <luisa.barros.dev@gmail.com>
2023-07-17 15:05:26 -0300

    feat: update navigators

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterWorkoutRecord from './screens/RegisterWorkoutRecord';
-import Workout from './screens/Workout';
+import theme from './themes';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <PaperProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Workout" component={Workout} />
-          <Stack.Screen
-            name="RegisterWorkoutRecord"
-            component={RegisterWorkoutRecord}
-          />
-        </Stack.Navigator>
-      </PaperProvider>
+      <ThemeProvider theme={theme}>
+        <PaperProvider>
+          <StatusBar barStyle={'light-content'} />
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}>
+            <Stack.Screen
+              name="RegisterWorkoutRecord"
+              component={RegisterWorkoutRecord}
+            />
+          </Stack.Navigator>
+        </PaperProvider>
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
