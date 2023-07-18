6344e4e2c8d528a4db4c9c89f434e10a6680c66b
Luisa Barros <luisa.barros.dev@gmail.com>
2023-07-17 21:09:37 -0300

    chore: update theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -4,6 +4,7 @@ const theme = {
     accent: '#DF1265',
     white: '#FFFFFF',
     disabled: '#D9D9D9',
+    secondary: '#979797',
     fonts: {
       primary: '#F2F2F2',
       secondary: '#828282'
