653d0b02f1fe945bf8b7665b54d4f18d88ffb0d4
Luisa Barros <luisa.barros.dev@gmail.com>
2023-07-17 21:10:05 -0300

    feat: create custom input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useTheme } from 'styled-components/native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Font } from '../';
+import { Container, Wrapper, CustomInput } from './styles';
+
+interface Props {
+  label: string;
+  icon?: React.ComponentProps<typeof Icon>['name'];
+  placeholder?: string;
+}
+
+const Input: React.FC<Props> = ({ label, icon, placeholder }) => {
+  const { colors } = useTheme();
+  return (
+    <Container>
+      <Font>{label}</Font>
+      <Wrapper>
+        <Icon size={24} name={icon || 'pencil'} color={colors.secondary} />
+        <CustomInput
+          placeholderTextColor={colors.secondary}
+          placeholder={placeholder || 'digite aqui'}
+        />
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: 5px 0;
+`;
+
+export const Wrapper = styled.View`
+  flex-direction: row;
+  padding-left: 10px;
+  margin-top: 5px;
+  align-items: center;
+  border-radius: 10px;
+  border: ${({ theme }) => `1px solid ${theme.colors.secondary}`};
+`;
+
+export const CustomInput = styled.TextInput`
+  flex: 1;
+  padding: 12px;
+  color: ${({ theme }) => theme.colors.fonts.primary};
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.fonts.main};
+`;
