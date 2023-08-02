8ff460550f8db8c416cb1f40fbb4828edc5eef3f
Luisa Barros <luisa.barros.dev@gmail.com>
2023-08-02 14:51:54 -0300

    feat: add workout weekdays proper placeholder and value

--- src/components/Accordion/styles.ts
+++ src/components/Accordion/styles.ts
@@ -1,12 +1,17 @@
-import styled from 'styled-components/native';
+import styled, { DefaultTheme } from 'styled-components/native';
 import { List } from 'react-native-paper';
+
+interface Props {
+  placeholder?: boolean;
+  theme: DefaultTheme;
+}
 
 export const Container = styled.View`
   margin: 5px 0;
 `;
 
 export const CustomAccordeon = styled(List.Accordion).attrs(
-  ({ theme, placeholder }) => ({
+  ({ theme, placeholder }: Props) => ({
     titleStyle: {
       color: placeholder
         ? theme.colors.fonts.secondary
@@ -16,12 +21,11 @@ export const CustomAccordeon = styled(List.Accordion).attrs(
     theme
   })
 )`
-  background-color: #333;
+  background-color: ${({ theme }: Props) => theme.colors.background};
   border-radius: 10px;
   padding: 2px 10px;
   font-size: 14px;
   margin-top: 5px;
-  color: orange;
   /* display: none; */
   border: ${({ theme }) => `1px solid ${theme.colors.secondary}`};
 `;
