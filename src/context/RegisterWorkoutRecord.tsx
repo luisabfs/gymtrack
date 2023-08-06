c1fa6ef88009e2efbde98f90b028ec818d686d0b
Luisa Barros <luisa.barros.dev@gmail.com>
2023-08-06 15:22:20 -0300

    feat[WIP]: add workout record context object

--- src/context/RegisterWorkoutRecord.tsx
+++ src/context/RegisterWorkoutRecord.tsx
@@ -1,27 +1,51 @@
-import React, {
-  createContext,
-  useState,
-  Dispatch,
-  SetStateAction
-} from 'react';
+import React, { createContext, useState } from 'react';
+
+interface Exercise {
+  name: string;
+  sets: number;
+  reps: number;
+}
+
+interface Workout {
+  name?: string;
+  muscleGroups?: string[];
+  workoutWeekdays?: string[];
+  exercises?: Exercise[];
+}
+
+interface WorkoutRecord {
+  name: string;
+  goal: string;
+  weekdays: string[];
+  workouts?: Workout;
+}
 
 interface RegisterWorkoutRecordContextData {
-  weekdayCheckbox: string[];
-  setWeekdayCheckbox: Dispatch<SetStateAction<string[]>>;
+  workoutRecord: WorkoutRecord;
+  setWorkoutRecord: (WorkoutRecord: WorkoutRecord) => void;
 }
 
 export const RegisterWorkoutRecordContext =
   createContext<RegisterWorkoutRecordContextData>({
-    weekdayCheckbox: [],
-    setWeekdayCheckbox: () => {}
+    workoutRecord: { name: '', goal: '', weekdays: [] },
+    setWorkoutRecord: () => {}
   });
 
-export const RegisterWorkoutRecordProvider = ({ children }) => {
-  const [weekdayCheckbox, setWeekdayCheckbox] = useState<string[]>([]);
+export const RegisterWorkoutRecordProvider: React.FC<{
+  children: React.ReactElement;
+}> = ({ children }) => {
+  const [workoutRecord, setWorkoutRecord] = useState<WorkoutRecord>({
+    name: '',
+    goal: '',
+    weekdays: []
+  });
 
   return (
     <RegisterWorkoutRecordContext.Provider
-      value={{ weekdayCheckbox, setWeekdayCheckbox }}>
+      value={{
+        workoutRecord,
+        setWorkoutRecord
+      }}>
       {children}
     </RegisterWorkoutRecordContext.Provider>
   );
