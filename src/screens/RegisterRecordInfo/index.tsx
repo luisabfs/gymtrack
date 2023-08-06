48a0763f18beb455b31e896c9b6834eea4285c44
Luisa Barros <luisa.barros.dev@gmail.com>
2023-08-06 15:27:16 -0300

    refactor: update register workout info screen

--- src/screens/RegisterRecordInfo/index.tsx
+++ src/screens/RegisterRecordInfo/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { View } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import { Font, Input, Accordion } from '../../components';
@@ -6,12 +6,25 @@ import { Button } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { RegisterWorkoutRecordNavigationProp } from '../../types/navigation.d';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { RegisterWorkoutRecordContext } from '../../context/RegisterWorkoutRecord';
 
 import { Container, ProgressBar } from './styles';
 
 const RegisterRecordInfo: React.FC = () => {
   const theme = useTheme();
   const navigation = useNavigation<RegisterWorkoutRecordNavigationProp>();
+  const { setWorkoutRecord, workoutRecord } = useContext(
+    RegisterWorkoutRecordContext
+  );
+
+  const [recordNameInput, setRecordNameInput] = useState('');
+  const [recordGoalInput, setRecordGoalInput] = useState('');
+
+  const validateInputs = (): boolean => {
+    return (
+      !!recordNameInput && !!recordGoalInput && !!workoutRecord.weekdays.length
+    );
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
@@ -52,12 +65,14 @@ const RegisterRecordInfo: React.FC = () => {
             leftIcon="pencil"
             label="nome"
             placeholder='"01/2023"'
+            onChangeText={(text) => setRecordNameInput(text)}
           />
           <Input
             rounded
             label="objetivo"
             placeholder='"Hipertrofia"'
             leftIcon="bullseye-arrow"
+            onChangeText={(text) => setRecordGoalInput(text)}
           />
 
           {/* // TODO: add date picker */}
@@ -71,10 +86,25 @@ const RegisterRecordInfo: React.FC = () => {
             style={{ borderRadius: 4, marginBottom: 5 }}
             mode="contained"
             buttonColor={theme.colors.accent}
-            onPress={() =>
-              navigation.navigate('RegisterWorkouts', { recordName: '01/2023' })
-            }>
-            <Font type="semibold">próximo →</Font>
+            theme={{ colors: { surfaceDisabled: theme.colors.disabled2 } }}
+            disabled={!validateInputs()}
+            onPress={() => {
+              setWorkoutRecord({
+                ...workoutRecord,
+                name: recordNameInput,
+                goal: recordGoalInput
+              });
+              navigation.navigate('RegisterWorkouts');
+            }}>
+            <Font
+              type="semibold"
+              color={
+                validateInputs()
+                  ? theme.colors.fonts.primary
+                  : theme.colors.fonts.secondary
+              }>
+              próximo →
+            </Font>
           </Button>
         </View>
       </Container>
